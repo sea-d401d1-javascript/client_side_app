3270cdc8b6462ee947df1a46b27367faeaf54a09
Ardian Ajvazi <ardian_46@hotmail.com>
2016-02-08 21:05:53 -0800

    displays current time and day

--- ardian/app/js/client.js
+++ ardian/app/js/client.js
@@ -1,1 +1,17 @@
 const angular = require('angular');
+const moment = require('moment');
+
+var getDay = () => {
+  var day = moment().format('dddd');
+  document.getElementById('day').innerHTML = day;
+};
+
+var getTime = () => {
+  var time = moment().format('MMMM Do YYYY, h:mm:ss a');
+  document.getElementById('time').innerHTML = time;
+  setInterval(getTime, 1000);
+};
+
+
+getDay();
+getTime();
