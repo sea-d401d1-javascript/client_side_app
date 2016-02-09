8b0a5121d86b9b4b59818a94448010538ad5bf60
jamesvermillion <jamesvermillion@yahoo.com>
2016-02-09 11:17:36 -0800

    some css included for more fun

--- jimVermillion/app/js/client.js
+++ jimVermillion/app/js/client.js
@@ -1,9 +1,12 @@
+'use strict';
+
+require('!style!css!../css/style.css');
 const moment = require('moment');
 const $ = require('jquery');
 
-(function(){
-  function getTime(){
-    $('#time').text((moment().format('MMMM Do YYYY, h:mm:ss a')));
+(function() {
+  function getTime() {
+    $('#time').text(moment().format('MMMM Do YYYY, h:mm:ss a'));
   }
   setInterval(getTime, 1000);
 })();
