838214a6b5d636346dacbfec4fb302d94332a5f7
jacethelefty <jacemontoya@live.com>
2016-02-09 13:10:57 -0800

    added setInterval 1000 so time works on Firefox too

--- jMontoya/app/scripts.js
+++ jMontoya/app/scripts.js
@@ -4,7 +4,7 @@ const $ = require('jquery');
 $(function(){
   setInterval(function(){
     $('#clock').text(moment().format("LTS"));
-  });
+  }, 1000);
 });
 
 $('#date').text(moment().format('MMMM Do YYYY'));
