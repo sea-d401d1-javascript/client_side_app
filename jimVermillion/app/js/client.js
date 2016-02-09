a161eb96e3f5b36096cf6e40d08d4f8542fd95ac
jamesvermillion <jamesvermillion@yahoo.com>
2016-02-09 10:06:04 -0800

    got jquery in node for fun

--- jimVermillion/app/js/client.js
+++ jimVermillion/app/js/client.js
@@ -1,9 +1,9 @@
 const moment = require('moment');
+const $ = require('jquery');
 
 (function(){
-  var time = document.getElementById('time');
   function getTime(){
-    time.textContent=moment().format('MMMM Do YYYY, h:mm:ss a');  
+    $('#time').text((moment().format('MMMM Do YYYY, h:mm:ss a')));
   }
   setInterval(getTime, 1000);
 })();
