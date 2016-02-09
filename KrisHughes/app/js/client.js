a4150588f650b9b5e482af0a706f0f9af45d4c1e
Kris <kristopher_hughes@yahoo.com>
2016-02-09 14:04:16 -0800

    homework

---
+++ KrisHughes/app/js/client.js
@@ -0,0 +1,12 @@
+const angular = require('angular');
+const moment = require('moment');
+
+angular.module('ngAppClock', []).controller('ngAppClockController', ['$scope', '$interval', function($scope, $interval) {
+
+  $interval(function() {
+	  $scope.clock = moment().format('MMMM Do YYYY, h:mm:ss a');
+  }, 1000);
+
+}]);
+
+
---
+++ KrisHughes/gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const webpack = require('webpack-stream');
+const eslint = require('gulp-eslint');
+
+gulp.task('html:dev', () => {
+	gulp.src(__dirname + '/app/**/*.html')
+	  .pipe(gulp.dest(__dirname + '/build'));
+});
+
+gulp.task('webpack:dev', () => {
+  gulp.src(__dirname + '/app/js/client.js')
+    .pipe(webpack({
+      output: {
+        filename: 'bundle.js'
+      }
+    }))
+    .pipe(gulp.dest('build/'));
+});
+
+gulp.task('build:dev', ['webpack:dev', 'html:dev']);
+gulp.task('default', ['build:dev']);
