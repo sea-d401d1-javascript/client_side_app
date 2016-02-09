024cd4c01a3216998be95d4ba37ee9889e599cc9
Joshua Ho <ho.joshua4@gmail.com>
2016-02-09 10:23:51 -0800

    First Commit - Deleting unused files

--- LoganTegman/app/index.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-import moment from 'moment';
-
-setInterval(() => {
-  document.getElementById('time')
-    .innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-}, 1000);
--- LoganTegman/server.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const express = require('express');
-
-express()
-  .use(express.static(__dirname + '/build'))
-  .listen(3000, () => console.log('Server running on port 3000'));
