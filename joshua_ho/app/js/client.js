f99fbec0d3df1bc1b0d30f8176c049a2082624b0
Joshua Ho <ho.joshua4@gmail.com>
2016-02-09 13:22:01 -0800

    Time is being returned!

--- joshua_ho/app/js/client.js
+++ joshua_ho/app/js/client.js
@@ -1,11 +1,16 @@
 const angular = require('angular');
 const moment = require('moment');
 
-var getTime = function (){
-  moment().format('MMMM Do YYYY, h:mm:ss a');
+//Special stuff I am learning
+// var myApp = angular.module('myApp' , []);
+
+(function getTime (){
+  var time = moment().format('MMMM Do YYYY, h:mm:ss a');
   console.log('Time Got-ted');
   console.log(time);
-};
+})();
+
+
 
 
 //Use set interval to have this run continuously, so updates occur
