85fdc1568c302066c2ac784caf4af4d296a31889
jessethach <jessethach@gmail.com>
2016-02-10 12:43:54 -0800

    added angular

--- jessethach/app/js/entry.js
+++ jessethach/app/js/entry.js
@@ -1,8 +1,22 @@
+const angular = require('angular');
 var moment = require('moment');
 
-function date_time() {
-  now = moment().format('dddd [d.] Do MMMM YYYY [kl.] HH:mm:ss');
-  document.getElementById('timer').innerHTML = now;
-  setTimeout(function () { date_time(); }, 1000);
-}
-date_time();
+const timeApp = angular.module('timeApp', []);
+
+timeApp.controller('timeAppController', ['$scope', '$interval', ($scope, $interval) => {
+  var tick = () => {
+    $scope.clock = $scope.newTime || moment().format('HH:mm:ss');
+  }
+  tick();
+  $interval(tick, 1000);
+  console.log($scope.clock);
+
+  $scope.addHour = () => {
+    $scope.newTime = moment().add(2, 'hours').format('HH:mm:ss');
+  };
+
+  $scope.subtractHour = () => {
+    $scope.newTime = moment().subtract(2, 'hours').format('HH:mm:ss');
+  };
+
+}]);
