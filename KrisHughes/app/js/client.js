e059722eb4a3a0a9f0b88e4907c19f6cb56b3763
Kris <kristopher_hughes@yahoo.com>
2016-02-10 12:16:49 -0800

    homework 2

--- KrisHughes/app/js/client.js
+++ KrisHughes/app/js/client.js
@@ -4,9 +4,13 @@ const moment = require('moment');
 angular.module('ngAppClock', []).controller('ngAppClockController', ['$scope', '$interval', function($scope, $interval) {
 
   $interval(function() {
+  	//Pacific Time
 	  $scope.clock = moment().format('MMMM Do YYYY, h:mm:ss a');
+	  //AK Time
+	  $scope.clockAK = moment().subtract(1, "h").format('MMMM Do YYYY, h:mm:ss a');
+	  //MT Time
+	  $scope.clockMT = moment().add(1, "h").format('MMMM Do YYYY, h:mm:ss a');
   }, 1000);
-
 }]);
 
 
--- KrisHughes/gulpfile.js
+++ KrisHughes/gulpfile.js
@@ -2,6 +2,11 @@ const gulp = require('gulp');
 const webpack = require('webpack-stream');
 const eslint = require('gulp-eslint');
 
+gulp.task('css:dev', () => {
+	gulp.src(__dirname + '/app/**/*.css')
+	  .pipe(gulp.dest(__dirname + '/build'));
+});
+
 gulp.task('html:dev', () => {
 	gulp.src(__dirname + '/app/**/*.html')
 	  .pipe(gulp.dest(__dirname + '/build'));
@@ -17,5 +22,5 @@ gulp.task('webpack:dev', () => {
     .pipe(gulp.dest('build/'));
 });
 
-gulp.task('build:dev', ['webpack:dev', 'html:dev']);
+gulp.task('build:dev', ['webpack:dev', 'css:dev', 'html:dev']);
 gulp.task('default', ['build:dev']);
