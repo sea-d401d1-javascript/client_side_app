3e289982208cf12d32e3568611237b379ff3b2b8
Christopher Lee <yensen@gmail.com>
2016-02-10 12:16:03 -0800

    working dropdown menu that selects/updates the displayed time

--- chris_lee/app/js/client.js
+++ chris_lee/app/js/client.js
@@ -4,15 +4,46 @@ const moment = require('moment');
 const app = angular.module('timeApp', []);
 
 app.controller('TimeController', ['$scope', ($scope) => {
-  $scope.changeZone = function() {
+  $scope.zones = [
+    {
+      city: 'Anchorage',
+      offset: -1
+    },
+    {
+      city: 'Seattle',
+      offset: 0
+    },
+    {
+      city: 'New York',
+      offset: 3
+    },
+    {
+      city: 'London',
+      offset: 8
+    },
+    {
+      city: 'Paris',
+      offset: 9
+    },
+    {
+      city: 'Taipei',
+      offset: 16
+    }
+  ];
+
+  $scope.changeZone = function(zone) {
+    var numHrs = zone.offset;
     clearTimeout($scope.timeout);
-    document.getElementById('time').innerHTML = moment().add(3,'h').format('MMMM Do YYYY, h:mm:ss a');
-    $scope.timeout = setTimeout(function () { $scope.changeZone(); }, 1000);
-  }
+    document.getElementById('time').innerHTML = 'Date/Time in ' + zone.city + ' is ' + moment().add(numHrs,'h').format('MMMM Do YYYY, h:mm:ss a');
+    $scope.timeout = setTimeout(function () { $scope.changeZone(zone); }, 1000);
+  };
+
   $scope.localTime = function() {
     clearTimeout($scope.timeout);
-    document.getElementById('time').innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+    document.getElementById('time').innerHTML = 'Date/Time in Seattle is ' + moment().format('MMMM Do YYYY, h:mm:ss a');
     $scope.timeout = setTimeout(function () { $scope.localTime(); }, 1000);
   };
-  $scope.localTime();
+
+  $scope.localTime();       // call localTime on page load
+
 }]);
