8020044524b1d73fbb142615e4b28147c0dada01
Joshua Ho <ho.joshua4@gmail.com>
2016-02-10 11:27:13 -0800

    Working application

--- joshua_ho/app/js/client.js
+++ joshua_ho/app/js/client.js
@@ -1,15 +1,37 @@
 const angular = require('angular');
 const moment = require('moment');
 
-//Special stuff I am learning
-// var myApp = angular.module('myApp' , []);
-
-(function getTime (){
+function getTime () {
   var time = moment().format('MMMM Do YYYY, h:mm:ss a');
-  console.log('Time Got-ted');
-  console.log(time);
-})();
+  return time;
+};
+// Special stuff I am learning
+var myApp = angular.module('myApp' , [])
+  .controller('TimeController', ['$scope',
+    ($scope) => {
+      $scope.format = 'M/m/yy h:mm:ss a';
+    }
+  ])
+  .directive('time' , ['$interval' ,
+    ($interval) => {
+      return (scope , element , attrs) => {
+        var stopTime;
+        function updateTime(){
+          element.text(moment().format('h:mm:ss a'));
+        }
+        scope.$watch(attrs.time , () => {
+          updateTime();
+        });
+        stopTime = $interval(updateTime , 1000);
+      };
+    }
+  ]);
 
+  // $scope.time = getTime();
+  // ( function() {
+  // $scope.time = getTime();
+  // return $scope.time;
+  // } , 1000);
 
 
 
