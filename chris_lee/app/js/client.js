7be8cd246216a2209bfb69eb4899037a082b777e
Christopher Lee <yensen@gmail.com>
2016-02-10 11:18:07 -0800

    update time works with controller, two working buttons to switch between new york and local time

--- chris_lee/app/js/client.js
+++ chris_lee/app/js/client.js
@@ -1,8 +1,18 @@
+const angular = require('angular');
 const moment = require('moment');
 
-var displayTime = function () {
-  document.getElementById('currentTime').innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
-  setTimeout(function () { displayTime(); }, 1000);
-}
+const app = angular.module('timeApp', []);
 
-displayTime();
+app.controller('TimeController', ['$scope', ($scope) => {
+  $scope.changeZone = function() {
+    clearTimeout($scope.timeout);
+    document.getElementById('time').innerHTML = moment().add(3,'h').format('MMMM Do YYYY, h:mm:ss a');
+    $scope.timeout = setTimeout(function () { $scope.changeZone(); }, 1000);
+  }
+  $scope.localTime = function() {
+    clearTimeout($scope.timeout);
+    document.getElementById('time').innerHTML = moment().format('MMMM Do YYYY, h:mm:ss a');
+    $scope.timeout = setTimeout(function () { $scope.localTime(); }, 1000);
+  };
+  $scope.localTime();
+}]);
