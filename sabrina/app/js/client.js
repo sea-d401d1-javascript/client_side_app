9fe03f74fe0a65054b40fff0d4191074296189bb
SABRINA TEE <sabrinatee@me.com>
2016-02-10 21:36:25 -0800

    Added timezone changes

--- sabrina/app/js/client.js
+++ sabrina/app/js/client.js
@@ -3,8 +3,27 @@ const moment = require('moment');
 const timeApp = angular.module('timeApp', []);
 
 timeApp.controller('timeController', ['$scope', '$interval', ($scope, $interval) => {
+  var currTime = moment(new Date());
+  var timeZone = parseInt(moment().format('Z'));
+
   $interval(() => {
-    var PST = moment().format('MMMM Do YYYY, h:mm:ss a');
-    $scope.time = PST;
+    updateTime();
+    $scope.time = currTime.format('MMMM Do YYYY, h:mm:ss a');
+    $scope.timezone = timeZone;
   }, 1000);
+
+  function updateTime() {
+    currTime.add($scope.count, 'hours');
+    $scope.count = 0;
+  }
+
+  $scope.addHour = () => {
+    $scope.count++;
+    timeZone++;
+  };
+
+  $scope.minusHour = () => {
+    $scope.count--;
+    timeZone--;
+  };
 }]);
