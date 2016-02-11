45756683cbf7dfc6333d85cc28224a45233d125f
Joshua Ho <ho.joshua4@gmail.com>
2016-02-10 16:42:24 -0800

    Finished assignment

--- joshua_ho/app/js/client.js
+++ joshua_ho/app/js/client.js
@@ -3,48 +3,26 @@ const moment = require('moment');
 
 // Special stuff I am learning
 var myApp = angular.module('myApp' , [])
-  .controller('TimeController', ['$scope', '$interval', ($scope , $interval) => {
+.controller('TimeController', ['$scope', '$interval', ($scope , $interval) => {
+  var currentTime;
+  var hourChange = 0;
+  var tick = function () {
+    currentTime = moment().add(hourChange , 'hour');
+    $scope.clock = currentTime.format("H:mm:ss a");
+  }
+  $scope.addTime = function () {
+    hourChange += 1;
+    currentTime = moment().add(hourChange , 'hour');
+    $scope.clock = currentTime.format("H:mm:ss a");
+  }
+  $scope.subtractTime = function () {
+    hourChange -= 1;
+    currentTime = moment().add(hourChange , 'hour');
+    $scope.clock = currentTime.format("H:mm:ss a");
+  }
 
-    var t = moment().format();
-    var tick = function () {
-      t = moment().format('HH:mm:ss a');
-      $scope.clock = $scope.newTime || t;
-    }
-    $scope.startTick = function() {
-      $interval(tick,1000);
-    }
-    $scope.addTime = function () {
-      $scope.newTime = t.add(1 , 'h');
-    }
-    $scope.subtractTime = function () {
-      t = moment().format().subtract(1 , 'H');
-      $scope.startTick();
-    }
-
-    $scope.startTick();
-
-    }
-  ]);
-  // .directive('time' , ['$interval' , function ($interval) {
-  //     return function (scope , element , attrs) {
-  //       var stopTime;
-  //       function updateTime(){
-  //         element.text(moment().format('h:mm:ss a'));
-  //       }
-  //
-  //       scope.$watch(attrs.time , () => {
-  //         updateTime();
-  //       });
-  //
-  //       stopTime = $interval(updateTime , 1000);
-  //
-  //     };
-  //   }
-  // ]);
-
-
-
-
-
+  tick();
+  $interval( tick , 1000 ) ;
+}]);
 //Use set interval to have this run continuously, so updates occur
 //on main JS file.
