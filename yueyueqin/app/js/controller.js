8d184eaa4f5ca1c883c6a920697235a18bd0046f
Yueyue07 <xww.qyy@gmail.com>
2016-02-10 17:19:43 -0800

    fix problem for adding or substracting 1 timezone

--- yueyueqin/app/js/controller.js
+++ yueyueqin/app/js/controller.js
@@ -3,32 +3,32 @@ var myApp = angular.module('myApp', []);
 myApp.controller('time', ['$scope', ($scope) => {
   $scope.format = 'yyyy-MM-ddTHH:mm:ssZ';
 }]).directive('myCurrentTime', (dateFilter) => {
-    return (scope, element, attrs) => {
-        var format;
+  return (scope, element, attrs) => {
+    var format;
 
-        scope.$watch(attrs.myCurrentTime, (value) => {
-            format = value;
-            currTime();
-        });
+    scope.$watch(attrs.myCurrentTime, (value) => {
+      format = value;
+      currTime();
+    });
         // watch the format change from input tag and then save it
 
-        function currTime() {
-            var dt = dateFilter(new Date(), format,'-0800');
-            element.text(dt);
+    function currTime() {
+      var dt = dateFilter(new Date(), format,'-0800');
+      element.text(dt);
             // element point to line 11 span element
-        }
+    }
 
-        function updateTime() {
-            setTimeout(() => {
-              currTime();
+    function updateTime() {
+      setTimeout(() => {
+        currTime();
               // update DOM
-              updateTime();
-              // schedule another update
-            }, 1000);
-        }
-
         updateTime();
-    };
+              // schedule another update
+      }, 1000);
+    }
+
+    updateTime();
+  };
 });
 
 myApp.controller('timeZone', ['$scope', ($scope) => {
@@ -45,10 +45,11 @@ myApp.controller('timeZone', ['$scope', ($scope) => {
     } else {
       alert('out of time zone range');
     }
+    console.log($scope.timezone);
   };
 
   $scope.minusOneHour = function() {
-    if ($scope.timezone) {
+    if ($scope.timezone || $scope.timezone === 0 ) {
       $scope.timezone = $scope.timezone;
     } else {
       $scope.timezone = -800;
@@ -60,17 +61,36 @@ myApp.controller('timeZone', ['$scope', ($scope) => {
     }
   };
 
-}]).directive('currentTimeZone', ['dateFilter', '$interval', (dateFilter,$interval) => {
+}]).directive('currentTimeZone', ['dateFilter', '$interval', (dateFilter, $interval) => {
   return (scope, element, attrs) => {
-    var timeoutId, utc;
+    var timeoutId, utc, timezoneString, zero, position;
     function updateTime() {
-      if (!scope.timezone.toString().length === 5 ){
-        var timezoneString = scope.timezone.toString();
-        var zero = '0';
-        var position = 1;
-        utc = [timezoneString.slice(0, position), zero, timezoneString.slice(position)].join('');
-      } else {
-        utc = scope.timezone.toString();
+      if (scope.timezone === 0) {
+        utc = '+0000';
+      }
+      if (scope.timezone < 0) {
+        if(scope.timezone.toString().length < 5){
+          timezoneString = scope.timezone.toString();
+          zero = '0';
+          position = 1;
+          utc = [timezoneString.slice(0, position), zero, timezoneString.slice(position)].join('');
+        }else{
+          utc = scope.timezone.toString();
+        }
+      }
+
+      if (scope.timezone > 0) {
+        if(scope.timezone.toString().length < 4) {
+          timezoneString = scope.timezone.toString();
+          zero = '+0';
+          position = 0;
+          utc = [timezoneString.slice(0, position), zero, timezoneString.slice(position)].join('');
+        }else{
+          timezoneString = scope.timezone.toString();
+          zero = '+';
+          position = 0;
+          utc = [timezoneString.slice(0, position), zero, timezoneString.slice(position)].join('');
+        }
       }
       element.text(dateFilter(new Date(), scope.format, utc));
     }
