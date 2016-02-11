2627efc16ba07fe6036a642e142a558f4bf2b6b1
Aaron Filson <aaronfilson@gmail.com>
2016-02-10 17:36:34 -0800

    Simple time updates on page.

--- AaronFilson/app/js/client.js
+++ AaronFilson/app/js/client.js
@@ -1,5 +1,15 @@
 const mom = require('moment');
 
-var currentTime = mom().format('MMMM Do YYYY, h:mm:ss a');
-currentTime.fake = null;
-document.write(currentTime);
+const angular = require('angular');
+
+const timeApp = angular.module('timeApp', []);
+
+timeApp.controller('timeController', ['$scope', ($scope) => {
+  var update = function() {
+    var myTag = document.getElementById('simpleTime');
+    myTag.innerHTML = mom().format('MMMM Do YYYY, h:mm:ss a');
+  };
+
+  window.setInterval(update, 1000);
+  $scope.fake = null; // be quiet linter, angular comes later
+}]);
