e24b8648b249aba0656ce70c8d8f69aef165699d
jessethach <jessethach@gmail.com>
2016-02-10 16:09:17 -0800

    fixed add hour function

--- jessethach/app/js/entry.js
+++ jessethach/app/js/entry.js
@@ -3,20 +3,24 @@ var moment = require('moment');
 
 const timeApp = angular.module('timeApp', []);
 
+//Advice taken from Jim Vermillion
 timeApp.controller('timeAppController', ['$scope', '$interval', ($scope, $interval) => {
+  $scope.count = 0;
   var tick = () => {
-    $scope.clock = $scope.newTime || moment().format('HH:mm:ss');
+    var time = moment(new Date());
+    time.add($scope.count, 'hours');
+    $scope.clock = time.format('HH:mm:ss')
   }
   tick();
   $interval(tick, 1000);
   console.log($scope.clock);
 
   $scope.addHour = () => {
-    $scope.newTime = moment().add(2, 'hours').format('HH:mm:ss');
+    $scope.count++;
   };
 
   $scope.subtractHour = () => {
-    $scope.newTime = moment().subtract(2, 'hours').format('HH:mm:ss');
+    $scope.count--;
   };
 
 }]);
