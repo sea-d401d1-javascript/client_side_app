e42af4f84006b7c68b2473f9a6ca701a1f7c7a4d
jessethach <jessethach@gmail.com>
2016-02-15 10:28:11 -0800

    initializing jedi routes on client

--- jessethach/app/js/entry.js
+++ jessethach/app/js/entry.js
@@ -1,26 +1,47 @@
 const angular = require('angular');
-var moment = require('moment');
 
-const timeApp = angular.module('timeApp', []);
+const jedisApp = angular.module('jedisApp', []);
 
-//Advice taken from Jim Vermillion
-timeApp.controller('timeAppController', ['$scope', '$interval', ($scope, $interval) => {
-  $scope.count = 0;
-  var tick = () => {
-    var time = moment(new Date());
-    time.add($scope.count, 'hours');
-    $scope.clock = time.format('HH:mm:ss')
+jedisApp.controller('jedisController', ['$scope', '$http', ($scope, $http) => {
+  $scope.greeting = 'hello world';
+  $scope.jedis = [];
+
+  $http.get('http://localhost:3000/api/jedis')
+    .then((res) => {
+      console.log('success!');
+      $scope.jedis = res.data;
+    }, (err) => {
+      console.log(err);
+    });
+
+  $scope.createJedi = function(jedi) {
+    $http.post('http://localhost:3000/api/jedis', jedi)
+      .then((res) => {
+        $scope.jedis.push(res.data);
+        $scope.newJedi = null;
+      }, (err) => {
+        console.log(err);
+      })
   }
-  tick();
-  $interval(tick, 1000);
-  console.log($scope.clock);
 
-  $scope.addHour = () => {
-    $scope.count++;
-  };
+  $scope.deleteJedi = function(jedi) {
+    $http.delete('http://localhost:3000/api/jedis/' + jedi._id)
+      .then((res) => {
+        $scope.jedis = $scope.jedis.filter((i) => i !== jedi);
+      }, (err) => {
+        console.log(err)
+      })
+  }
 
-  $scope.subtractHour = () => {
-    $scope.count--;
-  };
+  $scope.updateJedi = function(jedi) {
+    $http.put('http://localhost:3000/api/jedis/' + jedi._id, jedi)
+      .then((res) => {
+        $scope.jedis[$scope.jedis.indexof(jedi)] = jedi;
+        jedi.editting = false;
+      }, (err) => {
+        console.log(err);
+        $scope.editting = false;
+      })
+  }
 
 }]);
